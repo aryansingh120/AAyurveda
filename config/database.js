32071d66636e0547bd1021146bf38b42857b6aa7
aryansingh120 <aryanhp940@gmail.com>
2025-02-14 16:03:03 +0530

    Your commit message

--- config/database.js
+++ config/database.js
@@ -2,7 +2,11 @@ let mongoose=require("mongoose");
 
 let connectDb=async(req,res)=>{
     try {
-        await mongoose.connect(process.env.URI);
+        await mongoose.connect(process.env.URI,{
+            useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 50000, 
+        });
         
     } catch (error) {
         console.log("connection failed due to internal errror");
