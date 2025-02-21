a5da3c3b915156eefd803e580dd4df6452bcbaf0
aryansingh120 <aryanhp940@gmail.com>
2025-02-21 16:12:59 +0530

    middleware added

--- Routes/cartDetail.js
+++ Routes/cartDetail.js
@@ -1,8 +1,10 @@
 const express=require("express");
 const router=express.Router();
 const {cartDetails,allCart}=require("../controllers/cartDetails");
+const verifyToken=require("../middlewares/verifyToken")
 
-router.post("/cartadd",cartDetails);
+
+router.post("/cartadd",verifyToken,cartDetails);
 router.get("/allCart",allCart)
 
 module.exports=router
